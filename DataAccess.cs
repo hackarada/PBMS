bd2dc7af92b18d2808a4190fceb0dbe200a4420a
Ermias Bayu <ermi44@gmail.com>
2018-04-08 10:41:23 -0700

    Add files via upload
    Added Data-Layer and Business layer sample codes

---
+++ DataAccess.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    /// <summary>
+    /// this is A General Purpose DataAcess Class for all application Needs
+    /// </summary>
+    public class DataAccess
+    {
+        private SqlConnection _connection;
+        private SqlCommand _command;
+
+        private static string ConnectionString
+        {
+            get
+            {
+                string ConnectionString1 = (ConfigurationManager.ConnectionStrings["DALconnection"]).ToString();
+                return ConnectionString1;
+            }
+        }
+
+        private void Init(string commandText)
+        {
+            createCommand();
+            _command.CommandText = commandText;
+        }
+
+        private SqlCommand createCommand()
+        {
+            if (_command == null)
+            {
+                _command = new SqlCommand();
+            }
+            else
+            {
+                if (_command.Transaction != null)
+                {
+                    return _command;
+                }
+                else
+                {
+                    _command = new SqlCommand();
+                }
+            }
+
+            _command.Connection = createConnection();
+            return _command;
+        }
+
+        private SqlConnection createConnection()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqlConnection();
+                _connection.ConnectionString = ConnectionString;
+                _connection.Open();
+            }
+            else
+            {
+                if (_connection.State != ConnectionState.Open)
+                {
+                    _connection.Open();
+                }
+            }
+
+            return _connection;
+        }
+
+        public IDataReader loadDataByText(string commandText)
+        {
+            try
+            {
+                if (commandText.Equals(string.Empty))
+                    return null;
+
+                Init(commandText);
+                _command.CommandType = CommandType.Text;
+
+                return _command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public IDataReader loadDataByText(string commandText, SqlParameter[] paramList)
+        {
+            try
+            {
+                if (commandText.Equals(string.Empty) || paramList == null)
+                    return null;
+
+                Init(commandText);
+                _command.CommandType = CommandType.Text;
+                _command.Parameters.AddRange(paramList);
+
+                return _command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                //closeConnection();
+            }
+        }
+
+        public void saveByText(string commandText, SqlParameter[] paramList)
+        {
+            try
+            {
+                //  int status = 0;
+                if (commandText.Equals(string.Empty) || paramList == null)
+                    return;
+
+                Init(commandText);
+                _command.CommandType = CommandType.Text;
+                _command.Parameters.Clear();
+                _command.Parameters.AddRange(paramList);
+
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void ExcecuteProcedure(string ProcedureName, SqlParameter[] paramList)
+        {
+            try
+            {
+                //  int status = 0;
+                if (ProcedureName.Equals(string.Empty) || paramList == null)
+                    return;
+
+                Init(ProcedureName);
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.Parameters.Clear();
+                _command.Parameters.AddRange(paramList);
+
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void ExcecuteProcedure(string ProcedureName)
+        {
+            try
+            {
+                //  int status = 0;
+                if (ProcedureName.Equals(string.Empty))
+                    return;
+
+                Init(ProcedureName);
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void saveByText(string commandText)
+        {
+            //    int affected;
+            try
+            {
+                if (commandText.Equals(string.Empty))
+                    return;
+                Init(commandText);
+                _command.CommandType = CommandType.Text;
+                _command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public object ExcecuteScalar(string commandText, SqlParameter[] paramlist)
+        {
+            object x = new object();
+            try
+            {
+                if (commandText.Length != 0)
+                {
+                    Init(commandText);
+                    _command.CommandType = CommandType.Text;
+                    _command.Parameters.AddRange(paramlist);
+                    x = (_command.ExecuteScalar());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return x;
+        }
+
+        public object ExcecuteScalar(string commandText)
+        {
+            object x = new object();
+            try
+            {
+                if (commandText.Length != 0)
+                {
+                    Init(commandText);
+                    _command.CommandType = CommandType.Text;
+
+                    x = (_command.ExecuteScalar());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return x;
+        }
+
+        public void closeConnection()
+        {
+            try
+            {
+                if (_connection == null)
+                    return;
+
+                if (_connection.State == ConnectionState.Open)
+                {
+                    _connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Rpt_Transaction_Detail.svc.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using DAL;
+
+namespace BLservice
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Rpt_Transaction_Detail" in code, svc and config file together.
+    public class Rpt_Transaction_Detail
+    {
+        #region Default Constructor
+        DataAccess Dal;
+        public Rpt_Transaction_Detail()
+        {
+            Dal = new DataAccess();
+        }
+        #endregion
+
+        #region Public Properties
+
+        public int projectId { set; get; }
+        public string expenditureName { set; get; }
+        public string requesterFullName { set; get; }
+        public string withdrawReference { set; get; }
+        public string withdrawAmount { set; get; }
+        public string withdrawDate { set; get; }
+        public string withdrawReceiver { set; get; }
+
+        //int withdrawId { set; get; }
+        //string projectCode { set; get; }
+        public string projectName { set; get; }
+        public string StartDate { set; get; }
+        public string EndDate { set; get; }
+        public string ReportGeneratedDate { set; get; }
+        //int expenditureId { set; get; }
+        //int requestId { set; get; }
+        //int requesterId { set; get; }
+
+        #endregion
+
+        #region Methods
+        public List<Rpt_Transaction_Detail> LoadAllTransactions(int projectId, string startDate, string endDate, int expndtureType)
+        {
+            IDataReader reader = null;
+            DateConverter converterObj = new DateConverter();
+            string cmdTxt = "";
+            string gcStartDate = "", gcEndDate = "";
+            string expndtureString = "";
+
+            if (expndtureType != -1)
+                expndtureString = " And Expenditure_Id=" + expndtureType +" ";
+
+            if (!string.IsNullOrEmpty(startDate))
+                gcStartDate = converterObj.ConvertToGregorian(startDate);
+            else
+                startDate = "Not Selected";
+
+
+
+
+            if (!string.IsNullOrEmpty(endDate))
+                gcEndDate = converterObj.ConvertToGregorian(endDate);
+            else
+                endDate = "Not Selected";
+
+            if (projectId == -1 && startDate == "Not Selected" && endDate == "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Order By Withdraw_Date asc";
+
+            else if (projectId == -1 && startDate != "Not Selected" && endDate == "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Where Withdraw_Date >= '" + gcStartDate + "' " + expndtureString + "Order By Withdraw_Date asc";
+
+            else if (projectId == -1 && startDate == "Not Selected" && endDate != "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Where Withdraw_Date <= '" + gcEndDate + "' " + expndtureString + "Order By Withdraw_Date asc";
+
+            else if (projectId == -1 && startDate != "Not Selected" && endDate != "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Where Withdraw_Date BETWEEN '" + gcStartDate + "' And '" + gcEndDate + "' " + expndtureString + "Order By Withdraw_Date asc";
+
+            else if (projectId != -1 && startDate == "Not Selected" && endDate == "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Where Project_Id = " + projectId + " Order By Withdraw_Date asc";
+
+            else if (projectId != -1 && startDate != "Not Selected" && endDate == "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Where Project_Id = " + projectId + " And Withdraw_Date >= '" + gcStartDate + "' " + expndtureString + "Order By Withdraw_Date asc";
+
+            else if (projectId != -1 && startDate == "Not Selected" && endDate != "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Where Project_Id = " + projectId + " And Withdraw_Date <= '" + gcEndDate + "' " + expndtureString + "Order By Withdraw_Date asc";
+
+            else if (projectId != -1 && startDate != "Not Selected" && endDate != "Not Selected")
+                cmdTxt = "Select * From Rpt_Transaction_Detail Where Project_Id = " + projectId + " And Withdraw_Date BETWEEN '" + gcStartDate + "' And '" + gcEndDate + "' " + expndtureString + "Order By Withdraw_Date asc";
+
+
+            List<Rpt_Transaction_Detail> detailList = new List<Rpt_Transaction_Detail>();
+            try
+            {
+                Rpt_Transaction_Detail detailObj;
+                reader = Dal.loadDataByText(cmdTxt);
+                decimal withdrawAmountSum = 0;
+                while (reader.Read())
+                {
+                    detailObj = new Rpt_Transaction_Detail();
+                    ProjectBL projectObj = new ProjectBL();
+                    List<ProjectBL> proList = null;
+                    if (projectId != -1)
+                    {
+                        projectObj.Id = projectId;
+                        proList = projectObj.loadByID();
+                        detailObj.projectName = proList[0].Name;
+                    }
+                    else
+                        detailObj.projectName = "All Projects";
+
+                    detailObj.withdrawDate = converterObj.ConvertToEthiopian(reader["Withdraw_Date"].ToString());
+                    detailObj.requesterFullName = reader["Management_First_Name"].ToString() + " " + reader["Management_Last_Name"].ToString();
+                    detailObj.withdrawReference = reader["Withdraw_Reference_No"].ToString();
+                    detailObj.withdrawAmount = decimal.Parse(reader["Withdraw_Amount"].ToString()).ToString("N");
+                    detailObj.withdrawReceiver = reader["Received_By"].ToString();
+                    detailObj.expenditureName = reader["Expenditure_Name"].ToString();
+                    detailObj.StartDate = startDate;
+                    detailObj.EndDate = endDate;
+                    detailObj.ReportGeneratedDate = converterObj.ConvertToEthiopian(DateTime.Now.ToString());
+                    withdrawAmountSum += decimal.Parse(detailObj.withdrawAmount);    
+                    detailList.Add(detailObj);
+                }
+                detailObj = new Rpt_Transaction_Detail();
+
+                detailObj.withdrawDate = "----------------------\nTotal";
+                detailObj.requesterFullName = "--------------------------------\n";
+                detailObj.withdrawReference = "-----------------\n";
+                detailObj.withdrawAmount = "---------------------\n"+withdrawAmountSum.ToString("N");
+                detailObj.withdrawReceiver = "----------------------------\n";
+                detailObj.expenditureName = "-----------------------------\n";
+                detailList.Add(detailObj);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+                Dal.closeConnection();
+            }
+            return detailList;
+        }
+        #endregion
+    }
+}
